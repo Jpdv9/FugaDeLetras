a7aa8c9c44dab55f4d13d96ca8ea3d29f658ebe7
frank1192 <118237820+frank1192@users.noreply.github.com>
2023-04-26 11:39:54 -0500

    Se añade el link del video por olvido
    igualmente se evidencia en la carpeta de google drive que el archivo se subio antes de el tiempo estipulado

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,6 +4,7 @@ import Vistas.VistaInicio;
 
 /**
  * EN ESTA LINEA UBICAMOS EL LINK AL VIDEO DEL FUNCIONAMIENTO
+ * https://drive.google.com/drive/folders/18D3aBNtPlwAYa2xn9bUV2pQJ0CXMbYw2?usp=share_link
  * AUTORES
  * FRANKLIN AGUIRRE ORTIZ 201841743
  * JEAN PAUL DAVALOS VALENCIA 201832375
